8926ed82a5040717a95f741204e55893a4bfda55
saddamhr <saddambubt65@gmail.com>
2020-07-05 18:35:58 +0600

    searching functionality added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from './components/NavBar/NavBar';
 import ProductList from './components/ProductList/ProductList';
 import Cart from './components/Cart/Cart'
@@ -8,9 +8,17 @@ import data from './data'
 import './App.css';
 
 function App() {
+  const [products, setProduct] = useState([...data]);
+  const [keyword, setKeyword] = useState("");
+
+  useEffect(() => {
+    const results = data.filter(product => product.title.includes(keyword) || product.brand.includes(keyword));
+    setProduct(results);
+  }, [keyword]);
+
   return <div className="App">
-    <NavBar />
-    <ProductList products={data}/>
+    <NavBar setKeyword={setKeyword} />
+    <ProductList products={products}/>
     <Cart />
   </div>
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,8 +1,14 @@
 import React from "react"
 import './NavBar.css'
 
-const NavBar = () => {
-    return <div className="nav-bar">My Shop</div>;
+const NavBar = ({setKeyword}) => {
+    const handleChange = (e) => {
+        setKeyword(e.target.value);
+    };
+    return (<div className="nav-bar">
+        <span>My Shop</span>
+        <input placeholder="search" onChange={handleChange}/>
+    </div>);
 }
 
 export default NavBar;
