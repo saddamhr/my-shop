4115391105df458a08e52343109f49c90d84df57
saddamhr <saddambubt65@gmail.com>
2020-07-06 12:38:44 +0600

    checkout functionality added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './Cart.css'
 
 const CartItem = ({ id, title, price, quantity, removeCartItem }) => {
@@ -14,33 +14,52 @@ const CartItem = ({ id, title, price, quantity, removeCartItem }) => {
 };
 
 const Cart = ({ cartItems, removeCartItem, clearCart }) => {
+    const [checkoutOpen, setCheckoutOpen] = useState(false);
+    const [address, setAddress] = useState("");
+
+    const toggleCheckout = () => {
+        setCheckoutOpen(status => !status);
+    }
+
+    const handleChange = (e) => {
+        setAddress(e.target.value);
+    }
+
     const total = cartItems.reduce((sum, curr) => (sum + curr.price), 0);
     return (
         <div className="cart">
             <h4>Cart Items</h4>
             <div className="cart-items">
-            {cartItems.length === 0 && (<div className="cart-item">
+                {cartItems.length === 0 && (<div className="cart-item">
                     <div className="info">
                         <span>Cart is empty</span>
                     </div>
                 </div>)}
                 {cartItems.length !== 0 && cartItems.map(item => <CartItem {...item} removeCartItem={removeCartItem} />)}
-                {cartItems.length !== 0 && 
-                <>
-                <div className="cart-item">
-                    <div className="info">
-                        <span>Total</span>
-                        <span>{total}</span>
+                {cartItems.length !== 0 && (
+                    <>
+                        <div className="cart-item">
+                            <div className="info">
+                                <span>Total</span>
+                                <span>{total}</span>
+                            </div>
+                        </div>
+                        <div className="cart-item">
+                            <div className="info">
+                                <button style={{ color: 'white', backgroundColor: 'red' }} onClick={clearCart}>Cancel</button>
+                <button style={{ color: 'white', backgroundColor: 'green' }} onClick={toggleCheckout}>{checkoutOpen ? "Hide" : "Checkout"}</button>
+                            </div>
+                        </div>
+                    </>
+                )}
+                {cartItems.length !== 0 && checkoutOpen && (
+                    <div className="cart-item">
+                        <div className="info">
+                            <span><input placeholder="Address" onChange={handleChange} /></span>
+                            <button style={{ color: 'white', backgroundColor: !address ? 'gray' : 'green' }} disable={!address} onClick={clearCart}>Checkout</button>
+                        </div>
                     </div>
-                </div>
-                <div className="cart-item">
-                    <div className="info">
-                        <button style={{color: 'white', backgroundColor: 'red'}} onClick={clearCart}>Cancel</button>
-                        <button style={{color: 'white', backgroundColor: 'green'}}>Checkout</button>
-                    </div>
-                </div>
-                </>
-                }
+                )}
             </div>
         </div>
     );
