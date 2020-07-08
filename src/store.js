b9eb982850dfca460090a53269495787de145c0e
saddamhr <saddambubt65@gmail.com>
2020-07-08 12:15:01 +0600

    store creation

---
+++ src/store.js
@@ -0,0 +1,30 @@
+import React, { createContext, useReducer } from 'react';
+
+const init = {
+    cartItems: []
+};
+
+const store = createContext(init);
+const { Provider } = store;
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "SET_CART_ITEMS":
+            return {
+                ...state,
+                cartItems: action.payload
+            };
+        default:
+            return state;
+    }
+};
+
+const StateProvider = ({ children }) => {
+    const { state, dispatch } = useReducer(reducer, init);
+    return <Provider value={{ state, dispatch }}>{children}</Provider>
+}
+
+export {
+    store,
+    StateProvider
+};
